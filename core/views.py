336a2ca673baebc2deece8f68f297832f8030abd
Jota-Wolf <lobos.5769>
2019-10-11 21:06:33 -0300

    Creacion de proyecto WebPersonal

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about-me.html")
+
+def portafolio(request):
+    return render(request, "core/portafolio.html")
+
+def contacto(request):
+    return render(request, "core/contacto.html")
